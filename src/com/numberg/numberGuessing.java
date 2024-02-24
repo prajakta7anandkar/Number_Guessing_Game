0443f048dd0ed4858cf64d4840e0fd1d810bcb0e
Prajakta Anandkar <panandkar7@gmail.com>
2024-02-24 22:52:28 +0530

    number guessing

---
+++ src/com/numberg/numberGuessing.java
@@ -0,0 +1,60 @@
+package com.numberg;
+
+import java.util.Random; 
+import java.util.Scanner; 
+public class numberGuessing 
+{
+
+	public static void main(String[] args) 
+	{
+				// TODO Auto-generated method stub
+		
+				// stores actual and guess number 
+				int answer, guess; 
+
+				// maximum value is 100 
+				final int MAX = 100; 
+
+				// takes input using scanner 
+				Scanner in = new Scanner(System.in); 
+
+				// Random instance 
+				Random rand = new Random(); 
+
+				boolean correct = false; 
+
+				// correct answer 
+				answer = rand.nextInt(MAX) + 1; 
+
+				// loop until the guess is correct 
+				while (!correct) { 
+
+					System.out.println( 
+						"Guess a number between 1 and 100: "); 
+
+					// guess value 
+					guess = in.nextInt(); 
+
+					// if guess is greater than actual 
+					if (guess > answer) { 
+						System.out.println("Too high, try again"); 
+					} 
+
+					// if guess is less than actual 
+					else if (guess < answer) { 
+						System.out.println("Too low, try again"); 
+					} 
+
+					// guess is equal to actual value 
+					else { 
+
+						System.out.println( 
+							"Yes, you guessed the number."); 
+
+						correct = true; 
+					} 
+				} 
+				System.exit(0); 
+			} 
+		
+}
